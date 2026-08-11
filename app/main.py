402d2d91633ca24c8f52e8703224c2895144bcd9
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-08-11 10:30:42 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from database import Base, engine
 
-from routes import auth, vehicles, drivers
+from routes import auth, vehicles, drivers, trips
 
 
 Base.metadata.create_all(bind=engine)
@@ -16,6 +16,7 @@ app = FastAPI(
 app.include_router(auth.router)
 app.include_router(vehicles.router)
 app.include_router(drivers.router)
+app.include_router(trips.router)
 
 
 @app.get("/")
